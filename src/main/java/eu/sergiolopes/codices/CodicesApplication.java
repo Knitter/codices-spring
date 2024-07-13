a230f8b03a07f5337f9d80f9132870ed3e4a2ffb
Knitter <knitter.is@gmail.com>
2024-07-13 01:18:04 +0100

    Adds project files and basic structure

---
+++ src/main/java/eu/sergiolopes/codices/CodicesApplication.java
@@ -0,0 +1,13 @@
+package eu.sergiolopes.codices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/sergiolopes/codices/CodicesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sergiolopes.codices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
